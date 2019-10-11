dbd098494f138f514250b350e49e941ab415304a
Matt Gahrns <matthewgahrns@hotmail.com>
2019-10-11 14:59:55 -0700

    Done.

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,25 @@
 class FundingRound
 
+    attr_accessor :investment
+    attr_reader :startup, :venture_capitalist, :type
+    @@all = []
+
+    def initialize(venture_capitalist, type, investment, startup)
+        @investment = investment
+        @startup = startup
+        @venture_capitalist = venture_capitalist
+        @type = type
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+
+
+
+
+
+
 end
